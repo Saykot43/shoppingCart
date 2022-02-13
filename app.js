82ea627a76abcead35762e5caba3efdd7d0c7d94
Saykot Kumar Barmon <saykot.cse43@gmail.com>
2022-02-13 21:28:52 +0600

    common function add and phone part complte

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-function caseUpdate(isIncreasing){
-    const caseInput=document.getElementById('case-number');
-    let caseNumber = caseInput.value;
+function productUpdate(product, price, isIncreasing){
+    const productInput=document.getElementById(product+'-number');
+    let productNumber = productInput.value;
     // let caseNumber= parseInt( caseNumber);
     if(isIncreasing){
-        caseNumber= parseInt( caseNumber) +1;
-    }else if(caseNumber>0){
-        caseNumber= caseNumber -1;
+        productNumber= parseInt( productNumber) +1;
+    }else if(productNumber>0){
+        productNumber= productNumber -1;
     }
-    caseInput.value = caseNumber;
+    productInput.value = productNumber;
     // update case total
-    const caseTotal= document.getElementById('case-total');
-    caseTotal.innerText = caseNumber*59;
+    const productTotal= document.getElementById(product +'-total');
+    productTotal.innerText = productNumber*price;
 };
+// Phone part
+document.getElementById('phone-plus').addEventListener('click', function(){
+    productUpdate('phone',1200,true);
+});
+document.getElementById('phone-minas').addEventListener('click', function(){
+    productUpdate('phone', 1200, false);
+});
+
+
+// case part
 document.getElementById('case-plus').addEventListener('click', function(){
-    caseUpdate(true);  
+    productUpdate('case',50,true);  
 });
 
 document.getElementById('case-minas').addEventListener('click', function(){
-    caseUpdate(false);
+    productUpdate('case', 50, false);
 });
