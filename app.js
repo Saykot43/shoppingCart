6dc04dd63ade22da0c0616589862d61a592739b9
Saykot Kumar Barmon <saykot.cse43@gmail.com>
2022-02-13 20:56:41 +0600

    case part complte

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 function caseUpdate(isIncreasing){
     const caseInput=document.getElementById('case-number');
-    caseNumber= parseInt( caseInput.value);
+    let caseNumber = caseInput.value;
+    // let caseNumber= parseInt( caseNumber);
     if(isIncreasing){
-        caseInput.value= caseNumber +1;
+        caseNumber= parseInt( caseNumber) +1;
     }else if(caseNumber>0){
-        caseInput.value= caseNumber -1;
+        caseNumber= caseNumber -1;
     }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal= document.getElementById('case-total');
+    caseTotal.innerText = caseNumber*59;
 };
 document.getElementById('case-plus').addEventListener('click', function(){
     caseUpdate(true);  
